75808a8cc88472780d03e0e2d883d795995675db
auhmaugmaufm <supitcha.aq@gmail.com>
2025-04-21 22:46:29 +0700

    edit dashboard

--- frontend/src/components/ClientHome.tsx
+++ frontend/src/components/ClientHome.tsx
@@ -22,7 +22,7 @@ const ClientHome = () => {
 
 
     return (
-        <div className="flex flex-col  m-20 rounded-4xl bg-blue-100">
+        <div className="flex flex-col h-screen rounded-4xl justify-center items-center ">
             <div className="flex justify-center">
                 {data.length > 0 ? (
                     <ul className="flex flex-wrap gap-5 p-5 ">
--- frontend/src/components/CryptoCard.tsx
+++ frontend/src/components/CryptoCard.tsx
@@ -15,13 +15,23 @@ const CryptoCard = ({ coin }: { coin: Coin }) => {
       <div className="flex flex-row items-center gap-3">
         <img src={coin.image} alt={coin.id} className="w-8 h-8 object-contain" />
         <h3 className="text-xl font-bold">
-         {coin.name} ({coin.symbol.toUpperCase()})
+          {coin.name} ({coin.symbol.toUpperCase()})
         </h3>
       </div>
       <p>💰 Price: ${coin.current_price.toLocaleString()}</p>
-      <p>📉 24h Change: {coin.price_change_percentage_24h.toFixed(2)}%</p>
-      <p>📊 High (24h): ${coin.high_24h.toLocaleString()}</p>
-      <p>📊 Low (24h): ${coin.low_24h.toLocaleString()}</p>
+      <div className="flex flex-row">
+        <p>📉 24h Change: </p>
+        {coin.price_change_percentage_24h > 0 ?
+          <p className="text-emerald-600">{coin.price_change_percentage_24h.toFixed(2)}%</p> :
+          <p className="text-red-600">{coin.price_change_percentage_24h.toFixed(2)}%</p>
+        }
+      </div>
+      <div className="flex flex-row">
+        <p>📊 High (24h): </p><p className="text-emerald-600">${coin.high_24h.toLocaleString()}</p>
+      </div>
+      <div className="flex flex-row">
+        <p>📊 Low (24h): </p><p className="text-red-600">${coin.low_24h.toLocaleString()}</p>
+      </div>
     </li>
   );
 };
